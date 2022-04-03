ebb3f58ec9c0e407838cf5d47fd87d93acad2dc5
kisfelix <kisfelix1@gmail.com>
2022-04-03 19:56:06 +0200

    Feature - FileReader to read names.txt

---
+++ src/main/java/util/FileReader.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileReader {
+    private static final String fileName = "names.txt";
+
+    public static List<String> getNamesOfTxt() throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+        List<String> names = new ArrayList<>();
+        while (sc.hasNextLine())
+            names.add(sc.nextLine());
+        return names;
+    }
+}
