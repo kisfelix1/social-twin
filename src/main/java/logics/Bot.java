7f3e3d48539aa08f1b91b242ce0a812a85930ece
kisfelix <kisfelix1@gmail.com>
2022-04-03 19:58:17 +0200

    Feature - change Bot constructor to make driver always deny permission from chrome

--- src/main/java/logics/Bot.java
+++ src/main/java/logics/Bot.java
@@ -2,13 +2,21 @@ package logics;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Bot {
     protected static WebDriver driver;
 
     public Bot() {
+        Map<String, Object> prefs = new HashMap<String, Object>();
+        prefs.put("profile.default_content_setting_values.notifications", 2);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", prefs);
         setUpChromeDriverPath();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
