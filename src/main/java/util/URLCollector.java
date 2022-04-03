7b75f3b8ad5b0ff012cc918632b71138f9316baa
kisfelix <kisfelix1@gmail.com>
2022-04-03 20:31:44 +0200

    Feature - add Cleverbot page url to collector

--- src/main/java/util/URLCollector.java
+++ src/main/java/util/URLCollector.java
@@ -2,7 +2,8 @@ package util;
 
 public enum URLCollector {
     FACEBOOK_LOGIN_PAGE_URL("https://www.facebook.com/"),
-    MESSENGER_PAGE_URL("https://www.facebook.com/messages/");
+    MESSENGER_PAGE_URL("https://www.facebook.com/messages/"),
+    CLEVERBOT_PAGE_URL("https://www.cleverbot.com/");
     public final String URL;
 
     URLCollector(String url) {
