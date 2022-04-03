9bec8d1213414630ce60bd1f46d7b0ed4bca14c6
kisfelix <kisfelix1@gmail.com>
2022-04-03 18:57:52 +0200

    Feature - MessengerBot for future development

--- src/main/java/logics/MessengerBot.java
+++ src/main/java/logics/MessengerBot.java
@@ -1,12 +1,42 @@
 package logics;
 
-public class MessengerBot extends Bot{
-    public MessengerBot() {
+import pages.FacebookLoginPage;
 
+public class MessengerBot extends Bot{
+    FacebookLoginPage fBLoginPage;
+    public MessengerBot() {
+        fBLoginPage = new FacebookLoginPage(driver);
     }
 
     @Override
     public void login() {
+        String email = System.getenv("email");
+        String password = System.getenv("password");
+        fBLoginPage.actLogin(email, password);
+    }
+
+    @Override
+    void sendAnswer(String answer) {
+
+    }
+
+    @Override
+    String getAnswerFromBotByMessage(String message) {
+        return null;
+    }
+
+    @Override
+    String getMessage() {
+        return null;
+    }
+
+    @Override
+    boolean lastMessageInNameList() {
+        return false;
+    }
+
+    @Override
+    void goToMessages() {
 
     }
 }
