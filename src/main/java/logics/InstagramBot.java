aefeb2fd186c98d0d2c685b57e8963228af67f2b
kisfelix <kisfelix1@gmail.com>
2022-04-03 18:57:35 +0200

    Feature - InstagramBot for future development

--- src/main/java/logics/InstagramBot.java
+++ src/main/java/logics/InstagramBot.java
@@ -8,4 +8,29 @@ public class InstagramBot extends Bot {
     public void login() {
 
     }
+
+    @Override
+    void sendAnswer(String answer) {
+
+    }
+
+    @Override
+    String getAnswerFromBotByMessage(String message) {
+        return null;
+    }
+
+    @Override
+    String getMessage() {
+        return null;
+    }
+
+    @Override
+    boolean lastMessageInNameList() {
+        return false;
+    }
+
+    @Override
+    void goToMessages() {
+
+    }
 }
