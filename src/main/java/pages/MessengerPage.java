9ce336240f6404ff1239ed89def6ebb49b19b8e4
kisfelix <kisfelix1@gmail.com>
2022-04-03 21:23:55 +0200

    Feature - implement sendAnswer

--- src/main/java/pages/MessengerPage.java
+++ src/main/java/pages/MessengerPage.java
@@ -1,24 +1,40 @@
 package pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import util.WebElementWait;
 
 public class MessengerPage extends BasePage {
     public MessengerPage(WebDriver driver) {
         super(driver);
     }
 
-    @FindBy(xpath = "//h1[contains(text(),'Chatek')]")
-    WebElement chatHeader;
-
     @FindBy(xpath = "//span[@data-testid='timestamp']")
     WebElement timeStamp;
+
+    @FindBy(xpath = "//div[@role='gridcell']")
+    WebElement firstChatOnPage;
+
+    @FindBy(xpath = "(//div[@dir='auto'])[last()]")
+    WebElement lastMessage;
+
+    @FindBy(xpath = "//div[@role='textbox']")
+    WebElement messageInputField;
+
 
     public boolean checkTimeStamp() {
         String timeStampOfLastMessage = timeStamp.getText().split(" ")[0];
         String timeMeasure = timeStamp.getText().split(" ")[1];
         return Integer.parseInt(timeStampOfLastMessage) < 1 && timeMeasure.equals("p");
+    }
+
+    public void sendAnswer(String answer) {
+        firstChatOnPage.click();
+        WebElementWait.waitUntilVisible(driver, messageInputField);
+        messageInputField.sendKeys(answer);
+        messageInputField.sendKeys(Keys.ENTER);
     }
 
 
