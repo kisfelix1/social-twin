4d35c7c67c11d55e4c80accc57ac1cce1b92e066
kisfelix <kisfelix1@gmail.com>
2022-04-05 17:41:56 +0200

    Refactor - restructure BasePage

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,13 +1,18 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
-    protected final WebDriver driver;
+    protected WebDriver driver;
 
-    public BasePage(WebDriver driver) {
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver, this);
+    }
+
+    public BasePage() {
     }
 }
