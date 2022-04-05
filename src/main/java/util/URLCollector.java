c62eba585af61ea61f78930af0c3f6a14c482886
kisfelix <kisfelix1@gmail.com>
2022-04-05 17:40:11 +0200

    Refactor - delete unnecessary url from collector

--- src/main/java/util/URLCollector.java
+++ src/main/java/util/URLCollector.java
@@ -1,8 +1,7 @@
 package util;
 
 public enum URLCollector {
-    FACEBOOK_LOGIN_PAGE_URL("https://www.facebook.com/"),
-    MESSENGER_PAGE_URL("https://www.facebook.com/messages/"),
+    MESSENGER_LOGIN_PAGE_URL("https://www.messenger.com/"),
     CLEVERBOT_PAGE_URL("https://www.cleverbot.com/");
     public final String URL;
 
